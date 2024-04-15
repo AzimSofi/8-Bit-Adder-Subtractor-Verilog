639a4fad42eff4e93f7dd5295f3bb9c37f717ed5
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2024-04-15 17:28:41 +0800

    よいしょっ

--- full_adder.v
+++ full_adder.v
@@ -1,35 +1,13 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/09/2016 01:26:30 PM
-// Design Name: 
-// Module Name: full_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module full_adder(
     output cout,
     output sum,
-    input ain,
-    input bin,
+    input x,
+    input y,
     input cin
     );
-
-    //assign bin2 = cin^bin; // XORing the inputs to bin with cin. If 1, subtract; if 0, add
     
-    assign sum = ain^bin^cin;
-    assign cout = (ain&bin) | (ain&cin) | (bin&cin);
+    assign sum = x ^ y ^ cin;
+    assign cout = (x & y) | (x & cin) | (y & cin);
 endmodule
